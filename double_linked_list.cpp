55af1cd8d5b78ec09c8502ee437395ef621677f4
Izalhadji31 <ryzalhadji@gmail.com>
2023-05-25 10:27:31 +0700

    class DoubleLinkedList selesai

--- double_linked_list.cpp
+++ double_linked_list.cpp
@@ -14,7 +14,18 @@ class DoubleLinkedList {
 private:
     Node* START;
 public:
+    DoubleLinkedList();
+    void addNode();
+    bool search(int rollNo, Node** previous, Node** current);
+    bool deleteNode(int rollNo);
+    bool listEmpty();
+    void ascending();
+    void descending();
+    void hapus();
+    void searchData();
 };
+
+
 int main()
 {
     std::cout << "Hello World!\n";
